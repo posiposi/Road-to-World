939567199fa7c56efd0ae464b0cf49f3fb8c1ae7
daichi.sugiyama <caad12.di2@gmail.com>
2021-09-16 09:37:28 +0000

    bikes_reservations_migrate_complete

--- database/migrations/2021_09_16_181231_create_bikes_table.php
+++ database/migrations/2021_09_16_181231_create_bikes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBikesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('bikes', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
             $table->string('name');
-            $table->unsignedBigInteger('tel');
+            $table->string('brand');
+            $table->string('status');
+            $table->string('bike_address');
             $table->string('image')->nullable();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bikes');
     }
 }
