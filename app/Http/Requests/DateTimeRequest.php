d42270dd1ed5502f4fd872935b1b69d4fe2ed51b
daichi.sugiyama <caad12.di2@gmail.com>
2021-10-07 07:58:40 +0000

    add_ValidationMessage

--- app/Http/Requests/DateTimeRequest.php
+++ app/Http/Requests/DateTimeRequest.php
@@ -28,6 +28,8 @@ class DateTimeRequest extends FormRequest
             'start_time' => 'required|date_format:H:i',
             'end_date' => 'required|date',
             'end_time' => 'required|date_format:H:i',
+            'end_date_time' => 'after:start_date_time',
+            'start_date_time' => 'after:now',
         ];
     }
     
@@ -38,6 +40,18 @@ class DateTimeRequest extends FormRequest
             'start_time.required' => '開始時間は必須です。',
             'end_date.required' => '終了日は必須です。',
             'end_time.required' => '終了時間は必須です。',
+            'end_date_time.after' => '終了日時は開始日時より後日を指定してください。',
+            'start_date_time.after' => '開始日時は現在時刻よりも後日を指定してください。',
         ];
+    }
+    
+    protected function prepareForValidation()
+    {
+        $start_date_time = ($this->filled(['start_date', 'start_time'])) ? $this->start_date .' '. $this->start_time : '';
+        $end_date_time = ($this->filled(['end_date', 'end_time'])) ? $this->end_date .' '. $this->end_time : '';
+        $this->merge([
+           'start_date_time' => $start_date_time,
+           'end_date_time' => $end_date_time,
+        ]);
     }
 }
