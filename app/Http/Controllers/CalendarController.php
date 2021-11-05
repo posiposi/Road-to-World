678d5cd8a6b6b9d1a088ce467beeaf00f1ef0c3a
daichi.sugiyama <caad12.di2@gmail.com>
2021-11-05 13:34:23 +0000

    [Remove/#12 Calendars.show.phpからCalendar.phpへ名称変更し置き場もAppへ変更]

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Reservation;
 
 class CalendarController extends Controller
 {
-    public function show(){
-        
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function show()
+    {
+        $cal = new Calendar();
+        $tag = $cal->showCalendarTag($request->month,$request->year);
+        return view('calendars.show', ['cal_tag' => $tag]);
     }
 }
