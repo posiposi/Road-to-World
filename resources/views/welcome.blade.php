94f933358f688543686ffa72c246896ab922a31e
daichi.sugiyama <caad12.di2@gmail.com>
2021-11-08 04:14:17 +0000

    [Change/#21] カルーセルから文章削除

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,20 +6,13 @@
             <div class="container">
                 <div id="main_visual" class="carousel slide carousel-fade" data-ride="carousel" data-wrap="true">
                     <div class="carousel-inner">
+                        {{-- スライド01 --}}
                         <div class="carousel-item active">
                             <img class="img-fluid" src="https://bikeshare-bucket001.s3.ap-northeast-1.amazonaws.com/wallpaper2you_576757.jpg" alt="写真">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h2 class="text-dark">ようこそ自転車の世界へ！</h2>
-                                <p class="text-dark">一覧の中から気になった自転車をレンタル！世界へ漕ぎ出しましょう！</p>
-                            </div>
                         </div>
                         {{-- スライド02 --}}
                         <div class="carousel-item">
                             <img class="img-fluid" src="https://bikeshare-bucket001.s3.ap-northeast-1.amazonaws.com/gt_PC_W1920_H1080.png" alt="写真">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h2 class="text-dark">ようこそ自転車の世界へ！</h2>
-                                <p class="text-dark">一覧の中から気になった自転車をレンタル！世界へ漕ぎ出しましょう！</p>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -30,7 +23,8 @@
                 <div class="container">
                     <div class="row mb-4">
                         <div class="col-md-12 mb-3">
-                            <h3 class="mb-3">Road to Worldについて</h3>
+                            <h3 class="mb-3">ようこそ自転車の世界へ！</h3>
+                            <h4>一覧の中から気になった自転車をレンタル！世界へ漕ぎ出しましょう！</h4>
                             <p>このRoad to Worldは自転車レンタルを通じて世界へ走り出す人々の手助けをします。
                             購入・使用の敷居が高いロードバイクを借りることで新しい世界を見つけられるでしょう。
                             また、複数自転車を所有している人にはレンタル自転車を登録することで、
@@ -50,18 +44,10 @@
                         {{-- スライド01 --}}
                         <div class="carousel-item active">
                             <img class="img-fluid" src="https://bikeshare-bucket001.s3.ap-northeast-1.amazonaws.com/wallpaper2you_576757.jpg" alt="写真">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h2 class="text-dark">ようこそ自転車の世界へ！</h2>
-                                <p class="text-dark">一覧の中から気になった自転車をレンタル！世界へ漕ぎ出しましょう！</p>
-                            </div>
                         </div>
                         {{-- スライド02 --}}
                         <div class="carousel-item">
                             <img class="img-fluid" src="https://bikeshare-bucket001.s3.ap-northeast-1.amazonaws.com/gt_PC_W1920_H1080.png" alt="写真">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h2 class="text-dark">ようこそ自転車の世界へ！</h2>
-                                <p class="text-dark">一覧の中から気になった自転車をレンタル！世界へ漕ぎ出しましょう！</p>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -72,7 +58,8 @@
                 <div class="container">
                     <div class="row mb-4">
                         <div class="col-md-12 mb-3">
-                            <h3 class="mb-3">Road to Worldについて</h3>
+                            <h3 class="mb-3">ようこそ自転車の世界へ！</h3>
+                            <h4>一覧の中から気になった自転車をレンタル！世界へ漕ぎ出しましょう！</h4>
                             <p>このRoad to Worldは自転車レンタルを通じて世界へ走り出す人々の手助けをします。
                             購入・使用の敷居が高いロードバイクを借りることで新しい世界を見つけられるでしょう。
                             また、複数自転車を所有している人にはレンタル自転車を登録することで、
