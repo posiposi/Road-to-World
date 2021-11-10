9e2d1a4e804c5d594aeb1e0488fca3c6602f7b56
daichi.sugiyama <caad12.di2@gmail.com>
2021-11-10 02:37:21 +0000

    [Change]/#12 開始時間<select>タグへテスト変更

--- resources/views/bikes/index.blade.php
+++ resources/views/bikes/index.blade.php
@@ -25,8 +25,14 @@
                                 </ul>
                                 <ul class="list-group list-unstyled border border-dark mt-3">
                                     {!! Form::open(['route' => ['bikes.reservation', $bike->id]]) !!}
-                                        <li class="list-group-item">開始時間　<input type="date" name="start_date"><input type="time" class="ml-1" name="start_time"></li>
-                                        <li class="list-group-item">終了時間　<input type="date" name="end_date"><input type="time" class="ml-1" name="end_time"></li>
+                                        <li class="list-group-item">開始時間　<input type="date" name="start_date">
+                                        {{--<input type="time" class="ml-1" name="start_time">--}}
+                                        <select name="time">
+                                            <option value="10:00">10:00</option>
+                                        </select>
+                                        </li>
+                                        <li class="list-group-item">終了時間　<input type="date" name="end_date">
+                                        <input type="time" class="ml-1" name="end_time"></li>
                                         {!! Form::submit('予約', ['class' => 'btn btn-success btn-block']) !!}
                                     {!! Form::close() !!}
                                 </ul>
