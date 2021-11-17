6f2932dc53172d608da0008cd9eaf8cfa9763664
daichi.sugiyama <caad12.di2@gmail.com>
2021-11-17 00:52:33 +0000

    [Add]/#42 カレンダー仮置き、仮ルーティング設定

---
+++ resources/views/calendars/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <table border="0">
+            <tr>
+                <td>メニュー</td>
+                <td>値段</td>
+            </tr>
+        <tbody>
+            <tr>
+                <td>オムライス</td>
+                <td>700円</td>
+            </tr>
+                <td>ハンバーグ</td>
+                <td>900円</td>
+            </tr>
+            <tr>
+                <td>ナポリタン</td>
+                <td>600円</td>
+            </tr>
+        </tbody>
+    </table>
+@endsection
