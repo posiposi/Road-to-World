5c6f92ff9b45ed81d19f025f9f7384574a1bf622
daichi.sugiyama <caad12.di2@gmail.com>
2021-11-18 05:20:18 +0000

    [Change]/#42 カレンダー雛形枠変更

--- resources/views/calendars/index.blade.php
+++ resources/views/calendars/index.blade.php
@@ -1,23 +1,83 @@
 @extends('layouts.app')
 
 @section('content')
-    <table border="0">
+    <head>
+        <meta charset="utf-8">
+        <title>School timetable</title>
+        <style>
+        html {
+        font-family: sans-serif;
+        }
+        
+        table {
+        border-collapse: collapse;
+        border: 2px solid rgb(200,200,200);
+        letter-spacing: 1px;
+        font-size: 0.8rem;
+        }
+        
+        td, th {
+        border: 1px solid rgb(190,190,190);
+        padding: 10px 20px;
+        }
+        
+        td {
+        text-align: center;
+        }
+        
+        th.saturday {
+        color: blue;
+        }
+        
+        th.sunday {
+        color:red;
+        } 
+        
+        caption {
+        padding: 10px;
+        }
+        </style>
+    </head>
+    
+    <body>
+        <h1>予約状況カレンダー</h1>
+        
+        <table>
             <tr>
-                <td>メニュー</td>
-                <td>値段</td>
+                <th></th>
+                @for($i = 1; $i < 49; $i++)
+                    <th>{{ $i }}</th>
+                @endfor
+                <tr>
+                    <th>Mon</th>
+                </tr>
+                <tr>
+                    <th>Tues</th>
+                </tr>
+                <tr>
+                    <th>Wed</th>
+                </tr>
+                <tr>
+                    <th>Thurs</th>
+                </tr>
+                    <th>Fri</th>
+                <tr>
+                    <th class="saturday">Sat</th>
+                </tr>
+                <tr>
+                    <th class="sunday">Sun</th>
+                </tr>
             </tr>
-        <tbody>
+        @for($i = 0; $i < 20; $i++)
             <tr>
-                <td>オムライス</td>
-                <td>700円</td>
+                <th>
+                    {{ $i }}
+                </th>
             </tr>
-                <td>ハンバーグ</td>
-                <td>900円</td>
-            </tr>
-            <tr>
-                <td>ナポリタン</td>
-                <td>600円</td>
-            </tr>
-        </tbody>
-    </table>
+        @endfor
+        @for($i = 0; $i < 20; $i++)
+            <td>{{ $i }}</td>
+        @endfor        
+        </table>
+    </body>
 @endsection
