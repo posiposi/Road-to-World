669675b921611c137218850cedf2b1db637d6645
daichi.sugiyama <caad12.di2@gmail.com>
2021-11-19 10:55:01 +0000

    [Change]/#53 phpのバージョン変更

--- app/Http/Requests/DateTimeRequest.php
+++ app/Http/Requests/DateTimeRequest.php
@@ -24,24 +24,28 @@ class DateTimeRequest extends FormRequest
     public function rules()
     {
         return [
-            'start_date' => 'required|date',
+            /*'start_date' => 'required|date',
             'start_time' => 'required|date_format:H:i',
             'end_date' => 'required|date',
-            'end_time' => 'required|date_format:H:i',
-            'end_date_time' => 'after:start_date_time',
-            'start_date_time' => 'after:now',
+            'end_time' => 'required|date_format:H:i',*/
+            'end_date_time' => 'required|after:now|after:start_date_time',
+            'start_date_time' => 'required|after:now|before:end_date_time',
         ];
     }
     
     public function messages()
     {
         return [
-            'start_date.required' => '開始日は必須です。',
+            /*'start_date.required' => '開始日は必須です。',
             'start_time.required' => '開始時間は必須です。',
             'end_date.required' => '終了日は必須です。',
-            'end_time.required' => '終了時間は必須です。',
-            'end_date_time.after' => '終了日時は開始日時より後日を指定してください。',
+            'end_time.required' => '終了時間は必須です。',*/
+            'start_date_time.required' => '開始日時を入力してください。',
             'start_date_time.after' => '開始日時は現在時刻よりも後を指定してください。',
+            'end_date_time.after:start_date_time' => '終了日時は開始日時以後を指定してください。',
+            'end_date_time.after:now' => '終了日時は現在以後を入力してください。',
+            'end_date_time.required' => '終了日時を入力してください。',
+            'start_date_time.before' => '開始日時は終了日時以前を指定してください。',
         ];
     }
     
