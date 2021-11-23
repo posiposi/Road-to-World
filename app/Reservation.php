8aa7c7691d86f803cd6566687b80381b7883ae04
daichi.sugiyama <caad12.di2@gmail.com>
2021-11-23 00:52:09 +0000

    [Add]/#42 BikeとReservationモデルにリレーションを記述

--- app/Reservation.php
+++ app/Reservation.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     protected $fillable = ['payment'];
+    
+    //一対多の記述(予約は1台の自転車に従属)
+    public function bike()
+    {
+        return $this->belongsTo(Bike::class);
+    }
 }
