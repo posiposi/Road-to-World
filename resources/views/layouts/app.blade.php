ae6364476f24f80f5cda033ce815bf8937690a28
daichi.sugiyama <caad12.di2@gmail.com>
2021-11-29 05:30:14 +0000

    [Add]/#21 BootstrapをCDNからassetへ変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,13 +6,15 @@
         <meta charset="utf-8">
         <title>Road to World</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+        {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"> --}}
         
         {{-- datetimepicker読み込み --}}
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
         <script src="jquery.datetimepicker.js"></script>
         <link rel="stylesheet" href="jquery.datetimepicker.css">
         <link rel="stylesheet" href="{{ asset('css/welcomepage.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/carousel.css') }}">
     </head>
 
     <body>
