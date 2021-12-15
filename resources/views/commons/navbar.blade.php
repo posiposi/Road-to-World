cc530574cd089d0beb6b886e349c2909e3011b4c
daichi.sugiyama <caad12.di2@gmail.com>
2021-12-15 13:05:32 +0000

    [Fix/Change]/#91 ナビゲーションバーの配置とプルダウンメニューを修正

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,29 +1,30 @@
-<header class="mb-4">
-    <nav class="navbar navbar-expand-sm navbar-dark bg-warning">
-        {{-- トップページへのリンク --}}
-        <a class="navbar-brand" href="/">Road to World</a>
-
-        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="nav-bar">
-            <ul class="navbar-nav mr-auto"></ul>
-            <ul class="navbar-nav">
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-warning sticky-top">
+        <div class="container">
+            {{-- ブランド --}}
+            <a class="navbar-brand" href="/">Road to World</a>
+            {{-- 切り替えボタン --}}
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-bar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            {{-- ナビゲーション --}}
+            <div class="collapse navbar-collapse" id="nav-bar">
+                {{-- ナビゲーションメニュー --}}
                 {{-- ログイン中の場合 --}}
                 @if(Auth::check())
-                    <div class="row">
+                    {{-- 左側メニュー --}}
+                    <ul class="navbar-nav mr-auto">
                         <div class="btn-bikeregister">
                             {!! link_to_route('bikes.get', 'バイク登録', [], ['class' => 'btn btn-success']) !!}
-                        </div>
-                        <div class="btn-bikesindex mx-2">
                             {!! link_to_route('bikes.index', '貸出中バイク一覧', [], ['class' => 'btn btn-primary']) !!}
                         </div>
-                    
-                        {{-- ユーザ一覧ページへのリンク --}}
-                        {{--<li class="nav-item"><a href="#" class="nav-link">Users</a></li>--}}
+                    </ul>
+                    {{-- 右側メニュー --}}
+                    <ul class="navbar-nav">
                         <li class="nav-item dropdown">
-                            <a href="#" class="nav-link text-dark dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                            <a href="#" class="nav-link dropdown-toggle text-dark" data-toggle="dropdown">
+                            {{ Auth::user()->name }}
+                            </a>
                             <ul class="dropdown-menu dropdown-menu-right">
                                 {{-- ユーザ詳細ページへのリンク --}}
                                 <li class="dropdown-item">{!! link_to_route('users.index', 'My Page', [], ['class' => 'nav-link text-primary']) !!}</li>
@@ -32,17 +33,18 @@
                                 <li class="dropdown-item">{!! link_to_route('logout.get', 'ログアウト') !!}</li>
                             </ul>
                         </li>
-                    </div>
+                    </ul>
+                
                 {{-- 未ログインの場合 --}}
                 @else
-                    <div class="row">
-                        {{-- ユーザ登録ページへのリンク --}}
-                        <div class="btn-signup">{!! link_to_route('signup.get', 'ユーザ登録', [], ['class' => 'btn btn-success']) !!}</div>
-                        {{-- ログインページへのリンク --}}
-                        <div class="btn-login mx-2">{!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-primary']) !!}</div>
-                    </div>
+                    <ul class="navbar-nav mr-auto">
+                            <div class="btn-signup">
+                                {!! link_to_route('signup.get', 'ユーザ登録', [], ['class' => 'btn btn-success']) !!}
+                                {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-primary']) !!}
+                            </div>
+                    </ul>
                 @endif
-            </ul>
+            </div>
         </div>
     </nav>
 </header>
