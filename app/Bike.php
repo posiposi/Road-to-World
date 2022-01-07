854e9f90c852df77144458cb12c8d1382e60ab75
daichi.sugiyama <caad12.di2@gmail.com>
2022-01-07 11:12:13 +0900

    [FIx]/#139 モデルのPHPdocを修正

--- app/Bike.php
+++ app/Bike.php
@@ -11,29 +11,25 @@ class Bike extends Model
         'name', 'brand', 'status', 'bike_address', 'image_path', 'price', 'remark',
     ];
     
-    /*
-    * 一対多の記述(バイクは複数のユーザに従属)
-    */
+    /** 一対多の記述(バイクは複数のユーザに従属) */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
     
-    /*
-    *多対多の記述(多数のバイクが中間テーブルを通して多数のユーザに予約される。)
-    */
+    /** 多対多の記述(多数のバイクが中間テーブルを通して多数のユーザに予約される。) */
     public function reserved()
     {
         return $this->belongsToMany(User::class, 'reservations', 'bike_id', 'user_id');
     }
     
-    //一対多の記述(バイクは複数のコメントを所有)
+    /** 一対多の記述(バイクは複数のコメントを所有) */
     public function comments()
     {
         return $this->hasMany(Comment::class);
     }
     
-    //一対多の記述(バイクは複数の予約を所持)
+    /** 一対多の記述(バイクは複数の予約を所持) */
     public function reservations()
     {
         return $this->hasMany(Reservation::class);
