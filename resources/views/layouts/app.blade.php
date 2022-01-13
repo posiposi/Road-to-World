158c8fc5f32a059de259ac935046706c1c905d78
daichi.sugiyama <caad12.di2@gmail.com>
2022-01-13 10:51:08 +0900

    [Add]/#119 フラッシュメッセージにCSSを適用

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
 
         <!-- フラッシュメッセージ -->
         @if (session('flash_message'))
-            <div class="flash_message">
+            <div class="flash_message bg-danger text-center py-3 my-0">
                 {{ session('flash_message') }}
             </div>
         @endif
