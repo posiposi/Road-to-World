a8815bb28925e811dd39ea9d620556506729e37e
daichi.sugiyama <caad12.di2@gmail.com>
2022-01-31 15:39:08 +0900

    [Add]/#174 予約テーブル用シーダーを追加

---
+++ database/seeds/ReservationsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ReservationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $time = new Carbon('now');
+        for ($i=1; $i<=3; $i++) {
+            $reservation = [
+                'user_id' => 2,
+                'bike_id' => 1,
+                'start_at' => $time,
+                'end_at' => $time->addHours($i),
+                'created_at' => now(),
+                'updated_at' => now(),
+                'payment' => 1,
+            ];
+            \Illuminate\Support\Facades\DB::table('reservations')->insert($reservation);
+        }
+    }
+}
