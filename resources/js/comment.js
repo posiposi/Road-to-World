215d0a1a08cba6f2dc253c406e8274eee2d2187f
daichi.sugiyama <caad12.di2@gmail.com>
2022-05-12 08:34:54 +0900

    #209 タイポミスだが一旦統一のためrecieverにスペル統一

--- resources/js/comment.js
+++ resources/js/comment.js
@@ -7,15 +7,15 @@ $(function () {
         let user_comment = $("#comment-form").val();
         let location_url = $(location).attr('href').split('/', 6); //アクセスURLを取得
         let bikeId = location_url[4];
-        let receiverId = location_url[5];
+        let recieverId = location_url[5];
 
         $.ajax({
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             },
-            url: '/comments/' + bikeId + '/' + receiverId + '/store',
+            url: '/comments/' + bikeId + '/' + recieverId + '/store',
             type: 'POST',
-            data: {'bikeId' : bikeId, 'receiverId' : receiverId, 'body' : user_comment},
+            data: {'bikeId' : bikeId, 'recieverId' : recieverId, 'body' : user_comment},
             dataType: 'json',
         }).done(function(data) {
             $('#comment-view').html(data);
