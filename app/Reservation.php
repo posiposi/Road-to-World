008ab8af66b2097cb1a66e547b7e8e7933dd5dbd
daichi.sugiyama <caad12.di2@gmail.com>
2022-09-03 13:10:12 +0900

    #264 リレーションのPHPDocを記述

--- app/Reservation.php
+++ app/Reservation.php
@@ -8,11 +8,21 @@ class Reservation extends Model
 {
     protected $fillable = ['start_at', 'end_at', 'payment'];
     
+    /**
+     * リレーション：予約は1台の自転車に帰属する
+     *
+     * @return void
+     */
     public function bike()
     {
         return $this->belongsTo(Bike::class);
     }
 
+    /**
+     * リレーション：予約は1人のユーザーに帰属する
+     *
+     * @return void
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
