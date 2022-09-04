aa319d9eccdbcdf6d7e835dbd1f846a87f4a5657
daichi.sugiyama <caad12.di2@gmail.com>
2022-09-04 14:02:26 +0900

    #264 既存予約確認メソッドにコメントを追加

--- app/Reservation.php
+++ app/Reservation.php
@@ -39,13 +39,19 @@ class Reservation extends Model
      * @return array DBおよび支払い処理のための各情報
      */
     public function isExistsBikeReserved($request, $bike_id){
+        // ログインユーザーを取得
         $auth_id = Auth::id();
+        // 予約希望の対象自転車を取得
         $bike = Bike::find($bike_id);
+        // 予約開始の希望時間
         $start_time = new Carbon($request->start_date. ' ' .$request->start_time);
+        // 予約終了の希望時間
         $end_time = new Carbon($request->end_date. ' ' .$request->end_time);
+        // 予約時間の長さを計算(開始-終了)
         $carbon_diff = $start_time->diffInMinutes($end_time);
+        // 合計料金計算用に３０分単位で除算
         $time = $carbon_diff / 30;
-
+        // 予約希望の時間に既存の予約があるかを確認し取得
         $exists = Reservation::where([
             ['bike_id', $bike_id], ['start_at', '<', $end_time], ['end_at', '>', $start_time]
         ])->exists();
