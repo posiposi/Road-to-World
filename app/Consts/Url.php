273716499e28fc0555601611faef772f4f8363cf
daichi.sugiyama <caad12.di2@gmail.com>
2022-09-17 16:18:37 +0900

    #276 メインページのハードコーディングを修正

--- app/Consts/Url.php
+++ app/Consts/Url.php
@@ -8,4 +8,13 @@ class Url
     const URL_LIST = [
         's3' => 'https://bikeshare-bucket001.s3.ap-northeast-1.amazonaws.com/'
     ];
+
+    const PICTURE_ACCESS_LIST = [
+        'welcome_logo' => 'welcome/logomark.png',
+        'how_to_picuture1' => 'welcome/how_to_1.jpg',
+        'how_to_picuture2' => 'welcome/how_to_2.jpg',
+        'how_to_picuture3' => 'welcome/how_to_3.jpg',
+        'how_to_picuture4' => 'welcome/how_to_4.jpg',
+        'how_to_picuture5' => 'welcome/how_to_5.jpg',
+    ];
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,11 +12,11 @@
 @section('content')
     <main>
         <div class="container text-center">
-            <img src ="https://bikeshare-bucket001.s3.ap-northeast-1.amazonaws.com/welcome/logomark.png" alt="ロゴ">
+            <img src="{{ Url::URL_LIST['s3'] . Url::PICTURE_ACCESS_LIST['welcome_logo'] }}"
         </div>
         <div class="jumbotron">
             <div class="container">
-                <h2 class="main_visual_text">ようこそロードバイクの世界へ</h2>
+                <h2 class="main_visual_text">{{ Message::MAINPAGE_TEXT['main_title'] }}</h2>
             </div>
         </div>
         @guest
@@ -26,27 +26,22 @@
                         <div class="col-sm-3">
                             {{ Form::open(['route' => 'login.post']) }}
                                 <div class="form-group">
-                                    {{ Form::label('email', 'メールアドレス') }}
+                                    {{ Form::label('email', Word::WORD_LIST['email']) }}
                                     {{ Form::email('email', null, ['class' => 'form-control']) }}
                                 </div>
                                 <div class="form-group">
-                                    {{ Form::label('password', 'パスワード') }}
+                                    {{ Form::label('password', Word::WORD_LIST['password']) }}
                                     {{ Form::password('password', ['class' => 'form-control']) }}
                                 </div>
-                                {{ Form::submit('ログイン', ['class' => 'btn btn-primary btn-block']) }}
+                                {{ Form::submit(Word::WORD_LIST['login'], ['class' => 'btn btn-primary btn-block']) }}
                             {{ Form::close() }}
                             <div class="text-right mt-2">
-                                {{ link_to_route('signup.get', '初めての方はこちら', [], ['class' => 'text-dark']) }}
+                                {{ link_to_route('signup.get', Word::WORD_LIST['signup'], [], ['class' => 'text-dark']) }}
                             </div>
                         </div>
                         <div class="col-sm-9 mb-3 bg-white">
-                            <h4 class="mt-3">一覧の中から気になった自転車をレンタル！世界へ漕ぎ出しましょう！</h4>
-                            <p>Road to Worldは自転車レンタルを通じて世界へ走り出す人々の手助けをします。<br>
-                            購入・使用の敷居が高いロードバイクを借りることで新しい世界を見つけられるでしょう。<br>
-                            また、複数自転車を所有している人にはレンタル自転車を登録することで、
-                            購入費用の回収とロードバイクの世界を多くの人に紹介する機会になります。<br>
-                            アプリ製作者の理念は上記を通じて日本の人々にロードバイクの世界を知ってもらうことにあります。</p>
-                            <p>ロードバイクを借りて世界へ通じる'道'へ走り出しましょう！</p>
+                            <h4 class="mt-3">{{ Message::MAINPAGE_TEXT['sub_title'] }}</h4>
+                            <p>{!! nl2br(e(Message::MAINPAGE_TEXT['main_text'])) !!}</p>
                         </div>
                     </div>
                 </div>
@@ -54,8 +49,8 @@
             <section>
                 <ul class="navbar-nav">
                     <div class="btn-bikeregister mx-auto d-block mb-4">
-                        {{ link_to_route('bikes.index', '貸出中の自転車はこちらから', [], ['class' => 'btn btn-primary']) }}
-                        {{ link_to_route('search', '貸出中の自転車を検索する', [], ['class' => 'btn btn-info']) }}
+                        {{ link_to_route('bikes.index', Word::WORD_LIST['bikes_index'], [], ['class' => 'btn btn-primary']) }}
+                        {{ link_to_route('search', Word::WORD_LIST['search'], [], ['class' => 'btn btn-info']) }}
                     </div>
                 </ul>
             </section>
@@ -65,13 +60,8 @@
             <div class="container mt-4">
                 <div class="row mb-4">
                     <div class="col mb-3 bg-white">
-                        <h4 class="mt-3">一覧の中から気になった自転車をレンタル！世界へ漕ぎ出しましょう！</h4>
-                        <p>Road to Worldは自転車レンタルを通じて世界へ走り出す人々の手助けをします。<br>
-                        購入・使用の敷居が高いロードバイクを借りることで新しい世界を見つけられるでしょう。<br>
-                        また、複数自転車を所有している人にはレンタル自転車を登録することで、
-                        購入費用の回収とロードバイクの世界を多くの人に紹介する機会になります。<br>
-                        アプリ製作者の理念は上記を通じて日本の人々にロードバイクの世界を知ってもらうことにあります。</p>
-                        <p>ロードバイクを借りて世界へ通じる'道'へ走り出しましょう！</p>
+                        <h4 class="mt-3">{{ Message::MAINPAGE_TEXT['sub_title'] }}</h4>
+                        <p>{!! nl2br(e(Message::MAINPAGE_TEXT['main_text'])) !!}</p>
                     </div>
                 </div>
             </div>
@@ -95,7 +85,7 @@
                                         <div class="card-body">
                                             <h4 class="card-title">1.一覧から探す</h4>
                                         </div>
-                                        <img class="card-img-bottom" src="https://bikeshare-bucket001.s3.ap-northeast-1.amazonaws.com/welcome/how_to_1.jpg" alt="画像">
+                                        <img class="card-img-bottom" src={{ URL::URL_LIST['s3'] . URL::PICTURE_ACCESS_LIST['how_to_picuture1'] }} alt="画像">
                                     </div>
                                 </div>
                                 {{-- カード2 --}}
@@ -104,7 +94,7 @@
                                         <div class="card-body">
                                             <h4 class="card-title">2.登録内容を確認</h4>
                                         </div>
-                                        <img class="card-img-bottom" src="https://bikeshare-bucket001.s3.ap-northeast-1.amazonaws.com/welcome/how_to_2.jpg" alt="画像">
+                                        <img class="card-img-bottom" src={{ URL::URL_LIST['s3'] . URL::PICTURE_ACCESS_LIST['how_to_picuture2'] }} alt="画像">
                                     </div>
                                 </div>
                                 {{-- カード3 --}}
@@ -113,7 +103,7 @@
                                         <div class="card-body">
                                             <h4 class="card-title">3.予約確定</h4>
                                         </div>
-                                        <img class="card-img-bottom" src="https://bikeshare-bucket001.s3.ap-northeast-1.amazonaws.com/welcome/how_to_3.jpg" alt="画像">
+                                        <img class="card-img-bottom" src={{ URL::URL_LIST['s3'] . URL::PICTURE_ACCESS_LIST['how_to_picuture3'] }} alt="画像">
                                     </div>
                                 </div>
                                 {{-- カード4 --}}
@@ -122,7 +112,7 @@
                                         <div class="card-body">
                                             <h4 class="card-title">4.自転車をレンタル</h4>
                                         </div>
-                                        <img class="card-img-bottom" src="https://bikeshare-bucket001.s3.ap-northeast-1.amazonaws.com/welcome/how_to_4.jpg" alt="画像">
+                                        <img class="card-img-bottom" src={{ URL::URL_LIST['s3'] . URL::PICTURE_ACCESS_LIST['how_to_picuture4'] }} alt="画像">
                                     </div>
                                 </div>
                                 {{-- カード5 --}}
@@ -131,7 +121,7 @@
                                         <div class="card-body">
                                             <h4 class="card-title">5.自転車を返却</h4>
                                         </div>
-                                        <img class="card-img-bottom" src="https://bikeshare-bucket001.s3.ap-northeast-1.amazonaws.com/welcome/how_to_5.jpg" alt="画像">
+                                        <img class="card-img-bottom" src={{ URL::URL_LIST['s3'] . URL::PICTURE_ACCESS_LIST['how_to_picuture3'] }} alt="画像">
                                     </div>
                                 </div>
                             </div>
