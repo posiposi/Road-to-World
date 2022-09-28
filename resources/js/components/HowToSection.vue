0d14e4884a98fc4123f177841b2e2619c8995d38
daichi.sugiyama <caad12.di2@gmail.com>
2022-09-28 22:07:42 +0900

    #276 メインページのVue.js化テスト

--- resources/js/components/HowToSection.vue
+++ resources/js/components/HowToSection.vue
@@ -1,7 +1,33 @@
 <template>
-    <div v-for="(images) in ImagesForHowTo">
-        {{ images }}
-    </div>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="bg-warning py-4">
+                    <section class="how_to_title">
+                        <div class="row mb-4">
+                            <div class="col-md-8 mb-3">
+                                <h3 class="headline font-weight-bold ml-2">ご利用方法</h3>
+                            </div>
+                        </div>
+                        <div class="row mx-2">
+                            <div class="col-md-4 mb-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div v-for="(message) in Messages">
+                                            <h4 class="card-title">{{ message.id }} {{ message.text }}</h4>
+                                            <div v-for="(image) in ImagesForHowTo" :key="image.id">
+                                                <img :src=image alt="画像" class="card-img-bottom">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -11,8 +37,16 @@ import { reactive, ref, watch, onMounted } from "vue";
         props:['data'],
         setup(props) {
             const data = ref(props.data);
-            // 説明部画像を初期化し、設定する。
-            let ImagesForHowTo = ref([]);
+            // 使用方法エリアの画像を初期化し、設定する
+            let ImagesForHowTo = ref();
+            // 使用方法エリアで使用するテキストを設定する
+            let Messages = ref([
+                {id: 1, text: '一覧から探す'},
+                {id: 2, text: '登録内容を確認する'},
+                {id: 3, text: '予約確定'},
+                {id: 4, text: '自転車をレンタル'},
+                {id: 5, text: '自転車を返却'}
+            ]);
 
             const getImagesForHowTo = () => {
                 axios.get('/' + 'service/' + 'show')
@@ -29,7 +63,8 @@ import { reactive, ref, watch, onMounted } from "vue";
             return {
                 data,
                 ImagesForHowTo,
-                getImagesForHowTo
+                getImagesForHowTo,
+                Messages
             };
         }
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,6 @@
         <div class="container text-center">
             <img src="{{ Url::URL_LIST['s3'] . Url::PICTURE_ACCESS_LIST['welcome_logo'] }}">
         </div>
-        <how-to-section></how-to-section>
         <div class="jumbotron">
             <div class="container">
                 <h2 class="main_visual_text">{{ Message::MAINPAGE_TEXT['main_title'] }}</h2>
@@ -69,67 +68,6 @@
         </section>
         @endauth
         {{-- 利用方法 --}}
-        <section>
-            <div class="container">
-                <div class="row">
-                    <div class="bg-warning py-4">
-                        <section class="how_to">
-                            <div class="row mb-4">
-                                <div class="col-md-8 mb-3">
-                                    <h3 class="headline font-weight-bold ml-2">ご利用方法</h3>
-                                </div>
-                            </div>
-                            <div class="row mx-2">
-                                {{-- カード1 --}}
-                                <div class="col-md-4 mb-4">
-                                    <div class="card" style="width: max-width: 20rem;">
-                                        <div class="card-body">
-                                            <h4 class="card-title">1.一覧から探す</h4>
-                                        </div>
-                                        <img class="card-img-bottom" src={{ URL::URL_LIST['s3'] . URL::PICTURE_ACCESS_LIST['how_to_picuture1'] }} alt="画像">
-                                    </div>
-                                </div>
-                                {{-- カード2 --}}
-                                <div class="col-md-4 mb-4">
-                                    <div class="card" style="width: max-width: 20rem;">
-                                        <div class="card-body">
-                                            <h4 class="card-title">2.登録内容を確認</h4>
-                                        </div>
-                                        <img class="card-img-bottom" src={{ URL::URL_LIST['s3'] . URL::PICTURE_ACCESS_LIST['how_to_picuture2'] }} alt="画像">
-                                    </div>
-                                </div>
-                                {{-- カード3 --}}
-                                <div class="col-md-4 mb-4">
-                                    <div class="card" style="width: max-width: 20rem;">
-                                        <div class="card-body">
-                                            <h4 class="card-title">3.予約確定</h4>
-                                        </div>
-                                        <img class="card-img-bottom" src={{ URL::URL_LIST['s3'] . URL::PICTURE_ACCESS_LIST['how_to_picuture3'] }} alt="画像">
-                                    </div>
-                                </div>
-                                {{-- カード4 --}}
-                                <div class="col-md-4 mb-4">
-                                    <div class="card" style="width: max-width: 20rem;">
-                                        <div class="card-body">
-                                            <h4 class="card-title">4.自転車をレンタル</h4>
-                                        </div>
-                                        <img class="card-img-bottom" src={{ URL::URL_LIST['s3'] . URL::PICTURE_ACCESS_LIST['how_to_picuture4'] }} alt="画像">
-                                    </div>
-                                </div>
-                                {{-- カード5 --}}
-                                <div class="col-md-4 mb-4">
-                                    <div class="card" style="width: max-width: 20rem;">
-                                        <div class="card-body">
-                                            <h4 class="card-title">5.自転車を返却</h4>
-                                        </div>
-                                        <img class="card-img-bottom" src={{ URL::URL_LIST['s3'] . URL::PICTURE_ACCESS_LIST['how_to_picuture3'] }} alt="画像">
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <how-to-section></how-to-section>
     </main>
 @endsection
