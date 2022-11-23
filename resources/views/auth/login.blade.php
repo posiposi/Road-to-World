21860ec1e58bc951ed550223eedbbbdc80a41c02
daichi.sugiyama <caad12.di2@gmail.com>
2022-11-23 17:30:47 +0900

    #289 メインページからログインフォームを削除、ログイン画面に個別CSSを適用

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 
+@push('css')
+    <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
+@endpush
+
 @section('content')
     <div class="text-center my-4">
         <h1>ログイン</h1>
     </div>
 
+    {{-- 入力フォーム --}}
     <div class="row">
         <div class="col-sm-6 offset-sm-3">
-
             {{ Form::open(['route' => 'login.post']) }}
                 <div class="form-group">
                     {{ Form::label('email', 'メールアドレス') }}
@@ -18,8 +22,12 @@
                     {{ Form::password('password', ['class' => 'form-control']) }}
                 </div>
                 
-                {{ Form::submit('ログイン', ['class' => 'btn btn-success btn-block']) }}
-            {{ Form::close() }}
+                {{ Form::submit('ログイン', ['class' => 'login-btn btn d-block mx-auto rounded-pill']) }}
+
+                <p class="text-center">
+                    <a href="{{ route('signup.get') }}" id="register_account">アカウントを作成する</a>
+                </p>
+                {{ Form::close() }}
         </div>
     </div>
-@endsection('content')
+@endsection
