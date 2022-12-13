72bf1c941d64edcbce3e6553e184ae7ab3680373
daichi.sugiyama <caad12.di2@gmail.com>
2022-12-13 22:38:39 +0900

    #299 MDBフッターを実装

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,64 @@
-<footer id="footer">
-    <p class="copyright">© 2022 Daichi Sugiyama</p>
+<footer class="bg-secondary text-white text-center text-md-start">
+    <div class="container p-4 pb-2">
+        <div class="row mb-3">
+            <p class="d-flex justify-content-center align-items-center">
+                <span class="me-3">会員登録はこちらから</span>
+                <button type="button" class="btn btn-outline-light btn-rounded">
+                    Sign up!
+                </button>
+            </p>
+        </div>
+        <div class="row mb-2">
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Footer Content</h5>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+                    molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
+                    aliquam voluptatem veniam, est atque cumque eum delectus sint!
+                </p>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Links</h5>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!" class="text-white">Link 1</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="text-white">Link 2</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="text-white">Link 3</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="text-white">Link 4</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Links</h5>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!" class="text-white">Link 1</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="text-white">Link 2</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="text-white">Link 3</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="text-white">Link 4</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div style="text-align: center;">
+            <a class="btn text-white btn-floating m-1" style="background-color: #333333;" href="https://github.com/posiposi/Road-to-World">
+                <i class="fab fa-github"></i>
+            </a>
+        </div>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2022 Copyright: Daichi Sugiyama
+    </div>
 </footer>
