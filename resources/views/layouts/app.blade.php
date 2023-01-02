fa9c7bf4ef8e89d098a688307113b1dc7a19a1a4
daichi.sugiyama <caad12.di2@gmail.com>
2023-01-02 17:18:37 +0900

    #343 フッターをページ下部に固定するよう修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,13 +9,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/layouts_app.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/footer.css') }}">
     {{-- 個別CSS読み込み --}}
     @stack('css')
 </head>
 
-<body style="height: 100%; margin: 0">
-    <div class="wrapper" style="height: 100%; display: flex; flex-direction: column">
+<body>
+    <div class="wrapper">
         {{-- ナビゲーションバー --}}
         @include('commons.navbar')
 
@@ -27,15 +28,15 @@
         @endif
 
         {{-- 個別ページメインセクション --}}
-        <main style="flex: auto">
+        <main>
             {{-- エラーメッセージ --}}
             @include('commons.error_messages')
 
             @yield('content')
         </main>
-        {{-- フッター --}}
-        @include('layouts.footer')
     </div>
+    {{-- フッター --}}
+    @include('layouts.footer')
 
     <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
     <script src="{{ url(mix('js/app.js')) }}"></script>
