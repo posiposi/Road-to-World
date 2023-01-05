fecc54221fba40987f09b5d80b44fad4717360bc
daichi.sugiyama <caad12.di2@gmail.com>
2023-01-05 18:27:54 +0900

    #319 マイバイク画面の初期Mocを実装

---
+++ resources/views/bikes/mybike_index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <h1 class="font-weight-bold">{{ Word::PAGE_TITLE['mybike'] }}</h1>
+    </div>
+</div>
+
+@endsection
