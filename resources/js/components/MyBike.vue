bd8773e0225942825fb57e2d835395c42dca6780
daichi.sugiyama <caad12.di2@gmail.com>
2023-01-13 12:22:27 +0900

    #319 v-forループ内変数の名称を修正、カードタイトルとカード概要を設定

--- resources/js/components/MyBike.vue
+++ resources/js/components/MyBike.vue
@@ -1,17 +1,15 @@
 <template>
     <div class="row">
-        <div class="card" v-for="(image) in userBikes" :key="image">
+        <div class="card col-sm-4 mt-3" v-for="(userBike) in userBikes" :key="image">
             <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                <img :src="image.image_path" class="img-fluid" alt="自転車画像" />
+                <img :src="userBike.image_path" class="img-fluid" alt="自転車画像" />
                 <a href="#!">
                     <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
                 </a>
             </div>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
+                <h5 class="card-title">{{ userBike.brand }}</h5>
+                <p class="card-text">{{ userBike.remark }}</p>
                 <a href="#!" class="btn btn-primary">Button</a>
             </div>
         </div>
