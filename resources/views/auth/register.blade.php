872f123469b595443df9785ff61862e912497cc1
daichi.sugiyama <caad12.di2@gmail.com>
2023-01-21 00:24:27 +0900

    #346 Laravel Collectiveの一部を変更

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,41 +11,41 @@
     </div>
 
     <div class="row">
-        <div class="col-sm-6 offset-sm-3 mb-3">
+        <div class="col-sm-6 offset-sm-3">
+            <form action="{{ route('signup.post') }}" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="mb-3">
+                    {{ Form::label('name', '氏名') }}
+                    {{ Form::text('name', null, ['class' => 'form-control']) }}
+                </div>
 
-            {{ Form::open(['route' => 'signup.post']) }}
-            <div class="mb-3">
-                {{ Form::label('name', '氏名') }}
-                {{ Form::text('name', null, ['class' => 'form-control']) }}
-            </div>
+                <div class="mb-3">
+                    {{ Form::label('nickname', 'ニックネーム') }}
+                    {{ Form::text('nickname', null, ['class' => 'form-control']) }}
+                </div>
 
-            <div class="mb-3">
-                {{ Form::label('nickname', 'ニックネーム') }}
-                {{ Form::text('nickname', null, ['class' => 'form-control']) }}
-            </div>
+                <div class="mb-3">
+                    {{ Form::label('email', 'メールアドレス') }}
+                    {{ Form::email('email', null, ['class' => 'form-control']) }}
+                </div>
 
-            <div class="mb-3">
-                {{ Form::label('email', 'メールアドレス') }}
-                {{ Form::email('email', null, ['class' => 'form-control']) }}
-            </div>
+                <div class="mb-3">
+                    {{ Form::label('tel', '電話番号') }}
+                    {{ Form::text('tel', null, ['class' => 'form-control', 'placeholder' => 'ハイフン無しの半角数字で入力して下さい。'])}}
+                </div>
 
-            <div class="mb-3">
-                {{ Form::label('tel', '電話番号') }}
-                {{ Form::text('tel', null, ['class' => 'form-control', 'placeholder' => 'ハイフン無しの半角数字で入力して下さい。'])}}
-            </div>
+                <div class="mb-3">
+                    {{ Form::label('password', 'パスワード') }}
+                    {{ Form::password('password', ['class' => 'form-control', 'placeholder' => '8文字以上で入力して下さい。']) }}
+                </div>
 
-            <div class="mb-3">
-                {{ Form::label('password', 'パスワード') }}
-                {{ Form::password('password', ['class' => 'form-control', 'placeholder' => '8文字以上で入力して下さい。']) }}
-            </div>
+                <div class="mb-3">
+                    {{ Form::label('password_confirmation', 'パスワード確認') }}
+                    {{ Form::password('password_confirmation', ['class' => 'form-control']) }}
+                </div>
 
-            <div class="mb-3">
-                {{ Form::label('password_confirmation', 'パスワード確認') }}
-                {{ Form::password('password_confirmation', ['class' => 'form-control']) }}
-            </div>
-
-            {{ Form::submit('登録', ['class' => 'btn user-register-btn rounded-pill']) }}
-            {{ Form::close() }}
+                {{ Form::submit('登録', ['class' => 'btn user-register-btn rounded-pill']) }}
+            </form>
         </div>
     </div>
 </div>
