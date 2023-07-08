379e2bb8e428382a82cc4df3af71aaa608125bd0
daichi.sugiyama <caad12.di2@gmail.com>
2023-07-08 13:29:33 +0900

    インデント修正 #422

--- resources/ts/confirm-dialog.ts
+++ resources/ts/confirm-dialog.ts
@@ -2,15 +2,15 @@
  * 予約確認ダイアログを表示する
  */
 const showConfirmDialog = (event: Event) => {
-    if (window.confirm('予約してもよろしいですか？')) {
-        return true;
-    } else {
-        event.stopPropagation();
-        event.preventDefault();
-    }
+	if (window.confirm('予約してもよろしいですか？')) {
+		return true;
+	} else {
+		event.stopPropagation();
+		event.preventDefault();
+	}
 };
 
 // 複数の要素に同時にイベント登録はできないため、繰り返し処理でイベントを登録する。
 document.querySelectorAll('.reservation-form').forEach((button) => {
-    button.addEventListener('submit', showConfirmDialog);
+	button.addEventListener('submit', showConfirmDialog);
 });
