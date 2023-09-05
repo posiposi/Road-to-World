c9c56a03d8e84ca7abbe721e0c61e6abc377d20f
daichi.sugiyama <caad12.di2@gmail.com>
2023-09-05 20:47:14 +0900

    コメント送信時にDOMリストに送信日時を表示するように変更

--- resources/ts/sendMessage.ts
+++ resources/ts/sendMessage.ts
@@ -7,9 +7,11 @@ const inputMessage = <HTMLInputElement>
 	document.querySelector('#input-message');
 
 const addMessageList = (addMessage: string): void => {
-	let listBlock = document.querySelector('#list-block');
+	const listBlock = document.querySelector('#list-block');
 	let list: HTMLLIElement = document.createElement('li');
-	list.innerText = addMessage;
+	const DateTimeObject: Date = new Date();
+	const sendDateTime: string = DateTimeObject.toLocaleString('ja-JP', { month: "2-digit", day: "2-digit", hour: "numeric", minute: "numeric" });
+	list.innerText = sendDateTime + ' ' + addMessage;
 	listBlock?.appendChild(list);
 }
 
@@ -28,7 +30,6 @@ const listenMessageChannel = () => {
 	window.Echo.channel('message-added-channel').listen(
 		'MessageAdded',
 		(response) => {
-			console.log(response.message.body);
 			addMessageList(response.message.body);
 		}
 	);
