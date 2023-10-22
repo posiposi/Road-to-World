2b3b0f0678df22492e162866940868ae73dc8ded
daichi.sugiyama <caad12.di2@gmail.com>
2023-10-22 15:44:51 +0900

    サービスコンテナ登録 #445

--- app/Providers/CommentServiceProvider.php
+++ app/Providers/CommentServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Adapters\Comment\GetCommentAdapter;
+use App\Adapters\Comment\SaveCommentAdapter;
 use Core\src\Comment\UseCase\Ports\GetCommentQueryPort;
+use Core\src\Comment\UseCase\Ports\SaveCommentCommandPort;
 use Illuminate\Support\ServiceProvider;
 
 class CommentServiceProvider extends ServiceProvider
@@ -16,6 +18,7 @@ class CommentServiceProvider extends ServiceProvider
     public function register()
     {
         app()->bind(GetCommentQueryPort::class, GetCommentAdapter::class);
+        app()->bind(SaveCommentCommandPort::class, SaveCommentAdapter::class);
     }
 
     /**
