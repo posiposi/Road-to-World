a691b809d95db3c00bedc70e0e010bbf48e1848f
daichi.sugiyama <caad12.di2@gmail.com>
2023-10-22 09:44:57 +0900

    Commentドメインモデルに投稿日時を追加 #445

--- core/src/Comment/Domain/Models/Comment.php
+++ core/src/Comment/Domain/Models/Comment.php
@@ -2,6 +2,7 @@
 
 namespace Core\src\Comment\Domain\Models;
 
+use Carbon\Carbon;
 use Core\src\Bike\Domain\Models\BikeId;
 
 final class Comment
@@ -23,12 +24,18 @@ final class Comment
      */
     private $commentBody;
 
-    public function __construct($senderId, $receiverId, $bikeId, $commentBody)
+    /**
+     * @var Carbon
+     */
+    private $sendDateTime;
+
+    public function __construct($senderId, $receiverId, $bikeId, $commentBody, $sendDateTime)
     {
         $this->senderId = $senderId;
         $this->receiverId = $receiverId;
         $this->bikeId = $bikeId;
         $this->commentBody = $commentBody;
+        $this->sendDateTime  = $sendDateTime;
     }
 
     public function senderId(): SenderId
@@ -51,13 +58,18 @@ final class Comment
         return $this->commentBody;
     }
 
+    public function sendDateTime(): Carbon
+    {
+        return $this->sendDateTime;
+    }
     public static function fromArray(array $value): self
     {
         return new self(
             SenderId::of($value['sender_id'] ?? 0),
             ReceiverId::of($value['receiver_id'] ?? 0),
             BikeId::of($value['bike_id'] ?? 0),
-            CommentBody::of($value['body'] ?? '')
+            CommentBody::of($value['body'] ?? ''),
+            Carbon::now()
         );
     }
 }
